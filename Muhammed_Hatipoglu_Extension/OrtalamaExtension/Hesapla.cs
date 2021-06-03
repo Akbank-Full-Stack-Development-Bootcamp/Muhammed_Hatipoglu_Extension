93f3fc75aea8d739695935e3b5773abd00fef0ae
Muhammed <mhatipglu@gmail.com>
2021-06-04 00:37:57 +0300

    Add project files.

---
+++ Muhammed_Hatipoglu_Extension/OrtalamaExtension/Hesapla.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muhammed_Hatipoglu_Extension.OrtalamaExtension
+{
+    public static class Hesapla
+    {
+        public static string StringConversion(this int value) => Convert.ToString(value);
+    }
+}
---
+++ Muhammed_Hatipoglu_Extension/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Muhammed_Hatipoglu_Extension.OrtalamaExtension;
+
+namespace Muhammed_Hatipoglu_Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 3456;
+            string sonuc = value.StringConversion();
+            Console.WriteLine(sonuc +" "+ "değeri stringe çevrilmiştir.");
+        }
+    }
+}
